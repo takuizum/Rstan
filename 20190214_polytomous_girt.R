0ddc375bd8dadbc9547bbbfcb13d80ae00a33fcc
takuizum <sep10.taku.izum@gmail.com>
2019-02-16 00:45:50 +0900

    divide poly_girt.stan file into poly_girt.stan that is girt modeland grm.stan that is normal grm.

--- 20190214_polytomous_girt.R
+++ 20190214_polytomous_girt.R
@@ -1,13 +1,13 @@
 # polytomous stan
 library(tidyverse)
-
+library(rstan)
 Y <- ltm::Science %>% purrr::map_df(as.integer)
 
 stan_data <- list(N = nrow(Y), J = ncol(Y), Y = Y, K = 4)
 
 mod1 <- stan_model("poly_girt.stan")
 
-res1 <- sampling(mod1, data = stan_data, cores = 6, warmup = 2000, iter = 10000, control = list(adapt_delta = 0.99))
+res1 <- sampling(mod1, data = stan_data, cores = 6, warmup = 200, iter = 1000, control = list(adapt_delta = 0.99))
 
 # vb
 res2 <- vb(mod1, data = stan_data)
@@ -16,4 +16,23 @@ res2 <- vb(mod1, data = stan_data)
 library(shinystan)
 launch_shinystan(res1)
 
-res1 %>% rstan::extract() %>% purrr::map(as.matrix) %>% purrr::map(~apply(., 2, mean)) %>% head()
+# map function
+map <- function(z){ 
+  density(z)$x[which.max(density(z)$y)] 
+}
+
+# other of d
+res1_ex <- res1 %>% rstan::extract()
+res1_ex %>% purrr::map(as.matrix) %>% purrr::map(~apply(., 2, mean))
+
+# a
+a <- res1 %>% rstan::extract() %$% a %>% apply(2, mean)
+# d
+print(res1, pars = c("d"))
+beta <- res1 %>% rstan::extract() %$% d %>% apply(c(2,3), mean)
+
+theta <- res1 %>% rstan::extract() %$% theta %>% apply(2, mean)
+phi <- res1 %>% rstan::extract() %$% phi %>% apply(2, mean)
+
+cor(theta, phi)
+plot(theta, phi)
--- grm.stan
+++ grm.stan
@@ -1,25 +1,23 @@
 data {
   int<lower = 0> N; // n ob subjects
   int<lower = 0> J; // n of items
-  int<lower = 2> K;  // n of categories in each item
-  int<lower = 0, upper = K> Y[N, J];
+  int<lower = 2> K[J];  // n of categories in each item
+  int<lower = 0, upper = max(K)> Y[N, J];
 }
 
 parameters {
   vector[N] theta;
-  real<lower = 0> phi[N];
   real<lower = 0> a[J];
-  ordered[K-1]d[J]; // category of items
+  ordered[max(K)-1]d[J]; // category of items
   real mu_d;
   real<lower = 0> sigma_d;
 }
 
 model {
   a ~ lognormal(0, 1);
-  phi ~ lognormal(0, 1);
   theta ~ normal(0, 1);
   for(j in 1:J){
-    for(k in 1:(K-1)){
+    for(k in 1:(K[j]-1)){
       d[j, k] ~ normal(mu_d, sigma_d);
       // d[j, k] ~ normal(0,1);
     }
@@ -28,8 +26,7 @@ model {
   sigma_d ~ cauchy(0, 5);
   for(i in 1:N){
     for(j in 1:J){
-      Y[i,j] ~ ordered_logistic(1.702 * a[j]/sqrt(1 + phi[i]^2*a[j]^2) * theta[i], 1.702 * a[j]/sqrt(1 + phi[i]^2*a[j]^2)  * d[j]);
-       // Y[i,j] ~ ordered_logistic(a[j] * theta[i], d[j]);  // normal GRM
+      Y[i,j] ~ ordered_logistic(a[j] * theta[i], a[j] * d[j,1:(K[j]-1)]);  // normal GRM
     }
   }
 }
